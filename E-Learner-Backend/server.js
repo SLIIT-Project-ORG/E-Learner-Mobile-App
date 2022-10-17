bc5e94787782a43ec9208f4ad8e31d46616f19c8
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2022-10-17 23:20:33 +0530

    backend commit

---
+++ E-Learner-Backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
+require("./config/dbconnection");
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(PORT,()=>{
+    console.log(`App is running on port ${PORT}`);
+});
