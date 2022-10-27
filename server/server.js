52379899e8e94d740ce789c636b55951d5189e49
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2022-10-27 16:19:44 +0530

    course_mgmt_backend

--- server/server.js
+++ server/server.js
@@ -5,10 +5,13 @@ require("dotenv/config");
 require("./config/dbconnection");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
+
+const course = require("./routes/Course_Mgmt/courses.js");
+app.use("/course",course);
 
 app.listen(PORT,()=>{
     console.log(`App is running on port ${PORT}`);
